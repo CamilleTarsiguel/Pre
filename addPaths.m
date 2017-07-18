becc89305276f0dfc8836c8930658aae82ed6722
Camille Tarsiguel <camilletarsiguel@yahoo.fr>
2017-07-18 19:14:31 +0200

    Add files via upload

---
+++ addPaths.m
@@ -0,0 +1,10 @@
+function addPaths()
+% add necessary paths
+
+addpath(genpath('./energy'));
+addpath(genpath('./opengm'));
+addpath(genpath('./external'));
+addpath(genpath('./mex'));
+addpath(genpath('../motutils'));
+addpath(genpath('./motutils')); % only if installed on the fly
+end
